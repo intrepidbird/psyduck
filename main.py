d722d70df46ff004f6fc2d0c10315891c98ab4b4
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-09-29 16:01:53 -0400

    Psyduck OTZ

--- main.py
+++ main.py
@@ -2,30 +2,34 @@ import discord
 from discord import Intents
 from discord.ext import commands, tasks
 import wolframalpha
+from itertools import cycle
 
 intents = Intents.all()
 intents.messages = True
 status = cycle(['psyduck orz'])
+bot = commands.Bot(command_prefix='psyduck ', intents=intents)
 
 @bot.event
 async def on_ready(): change_status.start() 
-print("[+] Ready")
+print("Psyduck Ready! OTZ OTZ OTZ")
 
 @tasks.loop(seconds=10)
 async def change_status():
     await bot.change_presence(activity=discord.Game(next(status)))
-  
-bot = commands.Bot(command_prefix='psyduck ', intents=intents)
 
-client = wolframalpha.Client('client-id')
+client = wolframalpha.Client('id')
 
 @bot.command(name='orz')
 async def orz(ctx, *, question: str):
-    res = client.query(question)
-    if res['@success'] == 'false':
-        await ctx.send('I could not find an answer to your question.')
-    else:
-        answer = next(res.results).text
-        await ctx.send(f'The answer to your question is: {answer}')
+    try:
+        res = client.query(question)
+        if res['@success'] == 'false':
+            await ctx.send('Even with Psyduck having infinite Mathematical knowledge, the answer cannot be found...')
+        else:
+            answer = next(res.results).text
+            await ctx.send(f'Psyduck is so orz that he knows the answer is {answer}')
+    except Exception as e:
+        await ctx.send('Psyduck Errored')
 
-bot.run('your-bot-token')
+bot.run('token')
+
