02e9295c3ae93ec5d886a103521a6fb4982f30d7
Kartikey Bhatnagar <kartikey.bhatnagar2021@vitstudent.ac.in>
2023-10-03 01:58:11 +0530

    Updated main.py
    Added few changes to the bot 
    
    1) Error Handling: Added error handling for various cases, including command not found and missing required arguments.
    
    2) Custom Help Command: Added a custom !help command that provides a list of available commands and their descriptions using Discord embeds.
    
    3) Dice Rolling Command: Added a !roll command that allows users to roll a dice with a specified number of sides.
    
    4) Improved API Key Handling: Moved the API key settings to the top of the script for better organization.

--- main.py
+++ main.py
@@ -2,28 +2,45 @@ import discord
 from discord import Intents
 from discord.ext import commands, tasks
 import wolframalpha
-from itertools import cycle
 import openai
+import random
+import asyncio
+
+# Replace these with your actual API keys and bot token
+openai.api_key = 'your-openai-api-key'
+wolfram_api_key = 'your-wolfram-alpha-api-key'
+bot_token = 'your-bot-token'
 
 intents = Intents.all()
 intents.messages = True
-status = cycle(['psyduck orz'])
-bot = commands.Bot(command_prefix='psyduck ', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+status = cycle(['psyduck orz', 'I am a bot', 'Ask me anything'])
+
+# Error handling
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command not found. Type `!help` to see available commands.")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Missing required argument. Please check the command usage with `!help`.")
+    else:
+        await ctx.send(f"An error occurred: {error}")
 
 @bot.event
-async def on_ready(): change_status.start() 
-print("[+] Psyduck is ready")
+async def on_ready():
+    change_status.start()
+    print("[+] Psyduck is ready")
 
 @tasks.loop(seconds=10)
 async def change_status():
-    await bot.change_presence(activity=discord.Game(next(status)))
-
-openai.api_key = 'openai-api'
-client = wolframalpha.Client('wolfram-api')
+    new_status = next(status)
+    await bot.change_presence(activity=discord.Game(new_status))
 
 @bot.command(name='orz')
 async def orz(ctx, *, question: str):
     try:
+        client = wolframalpha.Client(wolfram_api_key)
         res = client.query(question)
         if res['@success'] == 'false':
             await ctx.send('Psyduck cannot find the answer')
@@ -31,7 +48,7 @@ async def orz(ctx, *, question: str):
             answer = next(res.results).text
             await ctx.send(f'Psyduck found out that the answer is {answer}')
     except Exception as e:
-        await ctx.send('Psyduck Errored')
+        await ctx.send('Psyduck encountered an error.')
 
 @bot.command(name='ai')
 async def gpt3(ctx, *, prompt: str):
@@ -39,6 +56,25 @@ async def gpt3(ctx, *, prompt: str):
         response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
         await ctx.send(response.choices[0].text.strip())
     except Exception as e:
-        await ctx.send('Psyduck Errored')
+        await ctx.send('Psyduck encountered an error.')
 
-bot.run('bot-token')
+# Custom help command
+@bot.command(name='help')
+async def help_command(ctx):
+    embed = discord.Embed(title="Psyduck Bot Commands", description="List of available commands:", color=0x00ff00)
+    embed.add_field(name="!orz <question>", value="Ask a question to Wolfram Alpha.", inline=False)
+    embed.add_field(name="!ai <prompt>", value="Generate text using GPT-3 AI.", inline=False)
+    embed.add_field(name="!roll <sides>", value="Roll a dice with the specified number of sides.", inline=False)
+    await ctx.send(embed=embed)
+
+# Command for rolling a dice
+@bot.command(name='roll')
+async def roll_dice(ctx, sides: int):
+    if sides <= 1:
+        await ctx.send("The dice must have at least 2 sides.")
+    else:
+        result = random.randint(1, sides)
+        await ctx.send(f"Rolled a {sides}-sided dice and got: {result}")
+
+# Run the bot
+bot.run(bot_token)
