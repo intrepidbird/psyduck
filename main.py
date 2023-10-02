82a053200d9149895d8b779de79870a9c3e3f3fa
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-10-02 15:55:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord import Intents
 from discord.ext import commands, tasks
 import wolframalpha
 from itertools import cycle
+import openai
 
 intents = Intents.all()
 intents.messages = True
@@ -11,25 +12,33 @@ bot = commands.Bot(command_prefix='psyduck ', intents=intents)
 
 @bot.event
 async def on_ready(): change_status.start() 
-print("Psyduck Ready! OTZ OTZ OTZ")
+print("[+] Psyduck is ready")
 
 @tasks.loop(seconds=10)
 async def change_status():
     await bot.change_presence(activity=discord.Game(next(status)))
 
-client = wolframalpha.Client('id')
+openai.api_key = 'openai-api'
+client = wolframalpha.Client('wolfram-api')
 
 @bot.command(name='orz')
 async def orz(ctx, *, question: str):
     try:
         res = client.query(question)
         if res['@success'] == 'false':
-            await ctx.send('Even with Psyduck having infinite Mathematical knowledge, the answer cannot be found...')
+            await ctx.send('Psyduck cannot find the answer')
         else:
             answer = next(res.results).text
-            await ctx.send(f'Psyduck is so orz that he knows the answer is {answer}')
+            await ctx.send(f'Psyduck found out that the answer is {answer}')
     except Exception as e:
         await ctx.send('Psyduck Errored')
 
-bot.run('token')
+@bot.command(name='ai')
+async def gpt3(ctx, *, prompt: str):
+    try:
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
+        await ctx.send(response.choices[0].text.strip())
+    except Exception as e:
+        await ctx.send('Psyduck Errored')
 
+bot.run('bot-token')
